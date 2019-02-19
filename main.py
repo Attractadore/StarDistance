949cad3ec29fc331182fced2cae257fed5ffda38
Michael <attractadore02@gmail.com>
2019-02-19 14:06:28 +0300

    Added propper graph

--- main.py
+++ main.py
@@ -5,18 +5,16 @@ import sys
 from math import sqrt
 from matplotlib import pyplot
 
-def distance(image,perc):
+
+def distance(image, perc):
     im_data = to_gs_arr(image,perc)
-    # print(im_data.shape)
     s = (im_data.shape[0], 1)
     im_data = numpy.squeeze(numpy.split(im_data, 2, axis=1)[0])
     m = numpy.median(im_data)/255
     d = [abs(m - x/255) for x  in im_data]
     fluc = numpy.average(d)
-    # print(numpy.average(d))
+    return fluc
 
-    # return 0
-    return  fluc
 
 def to_gs_arr(img, perc=0.5):
     im = Image.open(img).convert("LA")
@@ -26,30 +24,38 @@ def to_gs_arr(img, perc=0.5):
     return numpy.asarray(im.getdata())
 
 
-
 def main1():
     distance(sys.argv[1], float(sys.argv[2]))
 
     return 0
 
+
 def main():
     rf = sys.argv[1]
+    k = float(sys.argv[2])
     with open(rf, "r") as f:
-        x = []
-        y = []
+        distances = []
+        flucs = []
         for line in f:
-            i, d, s = line.replace("\n", "").split(" ")
-            d = float(d)
-            s = float(s)
+            inp = line.replace("\n", "").split(" ")
+            i = inp[0]
+            # d = inp[1]
+            # s = inp[2]
+            # i, d, s = line.replace("\n", "").split(" ")
+            if i == "#":
+                continue
+            d = float(inp[1])
+            s = float(inp[2])
             fluc = distance(i, s)
+            flucs.append(fluc)
+            distances.append(d)
             print("Distance {0} fluctuation {1}".format(d, fluc))
-            pyplot.scatter(d, fluc)
-            # x.append(d)
-            # y.append(fluc)
+            # pyplot.scatter(d, fluc)
+        m_fluc = numpy.median(flucs)
+        corrected_flucs = [(m_fluc + (x - m_fluc) * k) for x in flucs]
+        pyplot.ylim(0, 0.3)
+        pyplot.scatter(distances, corrected_flucs)
 
-        # x = numpy.asarray(x)
-        # y = numpy.asarray(y)
-        # pyplot.scatter(x, y)
         pyplot.show()
 
 
