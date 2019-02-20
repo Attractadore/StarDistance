9506b83ca7bca893b27cc4437c2fb0ee1adc8fa8
Michael <attractadore02@gmail.com@>
2019-02-20 13:01:50 +0300

    Added more data and graph plotting

--- main.py
+++ main.py
@@ -1,85 +1,88 @@
 import numpy
-import PIL
 from PIL import Image
 import sys
 from math import sqrt
 from matplotlib import pyplot
+from sklearn.linear_model import LinearRegression
+
+def measure_fluctuation_squares(img, perc, square_side=10):
+    im = load_gs_image(img, perc)
+    w, h = im.size
+    crop_w = int(w/square_side)
+    crop_h = int(h/square_side)
+    ims = [None] * (square_side ** 2)
+    for i in range(square_side ** 2):
+        cs_w = crop_w * (i % square_side)
+        cs_h = crop_h * (i // square_side)
+        cropped_im = im.crop((cs_w, cs_h, cs_w + crop_w, cs_h + crop_h))
+        ims[i] = cropped_im
+    im = None
+    flucs = [measure_fluctuation(im, perc) for im in ims]
+    return numpy.average(flucs)
 
 
-def distance(image, perc):
-    im_data = to_gs_arr(image, perc)
+def measure_fluctuation(img, perc):
+    if type(img) == str:
+        img = load_gs_image(img, perc)
+    im_data = numpy.asarray(img.getdata())
     im_data = numpy.squeeze(numpy.split(im_data, 2, axis=1)[0])
-    # im_center = to_gs_arr(image, 0.01)
-    # im_center = numpy.squeeze(numpy.split(im_center, 2, axis=1)[0])
-    # im_center = to_gs_arr(image, 0.1)
-    # im_center = numpy.squeeze(numpy.split(im_center, 2, axis=1)[0])
-    # while len(im_center) > 0:
-    #     im_data.remove(im_center.pop())
-    # print("Image size {0} center size {1}".format(im_data.size, im_center.size))
-    # im_data = list(im_data)
-    # im_center = list(im_center)
-    # while len(im_center) > 0:
-    #     print(len(im_center))
-    #     im_data.remove(im_center.pop())
-    # im_data = numpy.asarray(im_data)
-    # print("Resized image size {0}".format(im_data.size))
-
-    # m = numpy.median(im_data)/255
     m = numpy.average(im_data)/255
-    # m_c = numpy.average(im_center)/255
-    # d = [abs(m - x/255) for x in im_data]
     d = [abs(m - x/255) for x in im_data]
-    # d_c = [abs(m_c - x/255) for x in im_center]
     fluc = numpy.average(d)
-    # fluc_c = numpy.average(d_c)
     return fluc
-    # return (fluc * im_data.size - fluc_c * im_center.size) / (im_data.size - im_center.size)
 
 
-def to_gs_arr(img, perc=0.5):
+def load_gs_image(img, perc=0.5):
     im = Image.open(img).convert("LA")
     w, h = im.size
     ratio = sqrt(perc)
     im = im.crop((w * (1 - ratio) / 2, h * (1 - ratio) / 2, w * (1 + ratio) / 2, h * (1 + ratio) / 2))
-    return numpy.asarray(im.getdata())
-
-
-def main1():
-    distance(sys.argv[1], float(sys.argv[2]))
-
-    return 0
+    return im
 
 
 def main():
     rf = sys.argv[1]
-    k = float(sys.argv[2])
+    # k = float(sys.argv[2])
     with open(rf, "r") as f:
         distances = []
-        flucs = []
+        # fluc_funcs = [measure_fluctuation, measure_fluctuation_squares]
+        fluc_funcs = [measure_fluctuation_squares]
+        flucs = [list() for _ in range(len(fluc_funcs))]
         for line in f:
             inp = line.replace("\n", "").split(" ")
-            i = inp[0]
-            # d = inp[1]
-            # s = inp[2]
-            # i, d, s = line.replace("\n", "").split(" ")
-            if i == "#":
-                print("Ignoring {0}".format(inp[1]))
+            im = inp[0]
+            if im == "#":
+                print("{0} ignored".format(inp[1]))
                 continue
             d = float(inp[1])
-            s = float(inp[2])
-            # s = 0.2
-            fluc = distance(i, s)
-            # if i[:5] == "data/":
-            #     fluc *= 0.7
-            # fluc += 20/(d ** 2)
-            flucs.append(fluc)
-            distances.append(d)
-            print("{2} Distance {0} fluctuation {1}".format(d, fluc, i))
-            # pyplot.scatter(d, fluc)
-        m_fluc = numpy.median(flucs)
-        # corrected_flucs = [(m_fluc + (x - m_fluc) * k) for x in flucs]
+            p = float(inp[2])
+
+            try:
+                for i in range(len(fluc_funcs)):
+                    fluc_func = fluc_funcs[i]
+                    res = fluc_func(im, p)
+                    flucs[i].append(res)
+            except FileNotFoundError:
+                print("{0} not found".format(im))
+                continue
+
+            distances.append(float(inp[1]))
+            print("{0} at {1}k ly away".format(im, d))
+
+        pyplot.xlim(2, max(distances) + 10)
         pyplot.ylim(0, 0.3)
-        pyplot.scatter(distances, flucs)
+        # pyplot.ylim(0.01, 1)
+        # pyplot.xscale("log")
+        # pyplot.yscale("log")
+        inverse_distances = [1 / d for d in distances]
+        for f in flucs:
+            clf = LinearRegression()
+            clf.fit(numpy.asarray(inverse_distances).reshape(-1, 1), f)
+            test_data = numpy.linspace(0.01, max(distances) + 10, 500)
+            inverse_test_data = numpy.asarray([1/d for d in test_data])
+            test_flucs = clf.predict(inverse_test_data.reshape(-1, 1))
+            pyplot.plot(test_data, test_flucs)
+            pyplot.scatter(distances, f)
 
         pyplot.show()
 
